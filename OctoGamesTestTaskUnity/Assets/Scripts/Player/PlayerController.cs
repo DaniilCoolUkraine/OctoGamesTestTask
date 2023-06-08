3a3ddbf25f3bda16449d491c454f2732146c9c30
DaniilCoolUkraine <cd0games2000@gmail.com>
2023-06-08 13:39:40 +0300

    fixed cursor bub

--- OctoGamesTestTaskUnity/Assets/Scripts/Player/PlayerController.cs
+++ OctoGamesTestTaskUnity/Assets/Scripts/Player/PlayerController.cs
@@ -1,4 +1,5 @@
-﻿using Opsive.UltimateCharacterController.Traits;
+﻿using Opsive.Shared.Input;
+using Opsive.UltimateCharacterController.Traits;
 using QulisoftTestTask.ScriptableObjects;
 using UnityEngine;
 
@@ -8,14 +9,20 @@ namespace QulisoftTestTask.Player
     {
         [SerializeField] private EventSO _onEnemyKilled;
         [SerializeField] private EventSO _onPlayerDeath;
+
+        [SerializeField] private CharacterHealth _characterHealth;
+        [SerializeField] private UnityInput _unityInput;
         
         [SerializeField] private PlayerEnemyKilledCounter _enemyKilledCounter;
-        [SerializeField] private CharacterHealth _characterHealth;
 
         private void OnEnable()
         {
             _onEnemyKilled.OnInvoked += _enemyKilledCounter.EnemyKilled;
-            _characterHealth.OnDeathEvent.AddListener(((arg0, vector3, o) => _onPlayerDeath.Invoke()));
+            _characterHealth.OnDeathEvent.AddListener(((arg0, vector3, o) =>
+            {
+                _unityInput.DisableCursor = false;
+                _onPlayerDeath.Invoke();
+            }));
         }
 
         private void OnDisable()
